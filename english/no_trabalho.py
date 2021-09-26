19df1e3f033113bbf587f1e7034a8c477b42d539
Eduardo Pinho <86359636+ShyrTheFirst@users.noreply.github.com>
2021-09-25 21:43:13 -0300

    Update no_trabalho.py

--- english/no_trabalho.py
+++ english/no_trabalho.py
@@ -48,10 +48,56 @@ def trabalho():
      pygame.draw.rect(SCREEN,WHITE, reclamar_rect)
      SCREEN.blit(aguentar, (100,350))
      SCREEN.blit(reclamar, (100,400))
+     x_selecionar = 90
+     y_selecionar = 350
+     reta_selecionar = pygame.image.load(r'imagens\reta_selecionar.png')
+     SCREEN.blit(reta_selecionar, (x_selecionar,y_selecionar))
      pygame.display.update()
-     if pygame.mouse.get_pressed() == (1,0,0):
-         mouseposition = pygame.mouse.get_pos()
-         if aguentar_rect.collidepoint(mouseposition):
+
+     if event.key == pygame.K_DOWN and y_selecionar ==  350:
+      y_selecionar = 400
+      SCREEN.fill(BLACK)
+      SCREEN.blit(frase01, (100,100))
+      SCREEN.blit(frase02, (100,200))
+      SCREEN.blit(frase03, (100,300))
+      SCREEN.blit(aguentar, (100,350))
+      SCREEN.blit(reclamar, (100,400))
+      SCREEN.blit(reta_selecionar, (x_selecionar, y_selecionar))
+      pygame.display.update()
+     elif event.key == pygame.K_DOWN and y_selecionar == 400:
+      y_selecionar = 350
+      SCREEN.fill(BLACK)
+      SCREEN.blit(frase01, (100,100))
+      SCREEN.blit(frase02, (100,200))
+      SCREEN.blit(frase03, (100,300))
+      SCREEN.blit(aguentar, (100,350))
+      SCREEN.blit(reclamar, (100,400))
+      SCREEN.blit(reta_selecionar, (x_selecionar, y_selecionar))
+      pygame.display.update()
+     if event.key == pygame.K_UP and y_selecionar =350:
+      y_selecionar = 400
+      SCREEN.fill(BLACK)
+      SCREEN.blit(frase01, (100,100))
+      SCREEN.blit(frase02, (100,200))
+      SCREEN.blit(frase03, (100,300))
+      SCREEN.blit(aguentar, (100,350))
+      SCREEN.blit(reclamar, (100,400))
+      SCREEN.blit(reta_selecionar, (x_selecionar, y_selecionar))
+      pygame.display.update()
+     elif event.key == pygame.K_UP and y_selecionar == 400:
+      y_selecionar = 350
+      SCREEN.fill(BLACK)
+      SCREEN.blit(frase01, (100,100))
+      SCREEN.blit(frase02, (100,200))
+      SCREEN.blit(frase03, (100,300))
+      SCREEN.blit(aguentar, (100,350))
+      SCREEN.blit(reclamar, (100,400))
+      SCREEN.blit(reta_selecionar, (x_selecionar, y_selecionar))
+      pygame.display.update()
+      #identificar qual botao esta sendo apertado:
+     if event.key == pygame.K_RETURN:
+      localdoy = y_selecionar
+      if localdoy == 350:
              v.depre += 1
              frase04 = fonte.render("You're feeling worst", 1, WHITE)
              SCREEN.blit(frase04, (100,500))
@@ -66,8 +112,7 @@ def trabalho():
              v.pe = False
              v.no_trabalho = False
              v.jantar = True
-             
-         if reclamar_rect.collidepoint(mouseposition):
+      If localdoy == 400:
              v.depre -= 1
              frase04 = fonte.render("You're feeling better", 1, WHITE)
              SCREEN.blit(frase04, (100,500))
@@ -82,9 +127,6 @@ def trabalho():
              v.pe = False
              v.no_trabalho = False
              v.jantar = True
-             
-     
-     
-     
 
-
+     
+     
