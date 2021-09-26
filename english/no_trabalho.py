0cd25269eac52151d685547abb7d9bde0197494b
Eduardo Pinho <86359636+ShyrTheFirst@users.noreply.github.com>
2021-09-25 22:14:20 -0300

    final update

--- english/no_trabalho.py
+++ english/no_trabalho.py
@@ -53,51 +53,83 @@ def trabalho():
      reta_selecionar = pygame.image.load(r'imagens\reta_selecionar.png')
      SCREEN.blit(reta_selecionar, (x_selecionar,y_selecionar))
      pygame.display.update()
+while v.no_trabalho == True:
+     for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-     if event.key == pygame.K_DOWN and y_selecionar ==  350:
-      y_selecionar = 400
-      SCREEN.fill(BLACK)
-      SCREEN.blit(frase01, (100,100))
-      SCREEN.blit(frase02, (100,200))
-      SCREEN.blit(frase03, (100,300))
-      SCREEN.blit(aguentar, (100,350))
-      SCREEN.blit(reclamar, (100,400))
-      SCREEN.blit(reta_selecionar, (x_selecionar, y_selecionar))
-      pygame.display.update()
-     elif event.key == pygame.K_DOWN and y_selecionar == 400:
-      y_selecionar = 350
-      SCREEN.fill(BLACK)
-      SCREEN.blit(frase01, (100,100))
-      SCREEN.blit(frase02, (100,200))
-      SCREEN.blit(frase03, (100,300))
-      SCREEN.blit(aguentar, (100,350))
-      SCREEN.blit(reclamar, (100,400))
-      SCREEN.blit(reta_selecionar, (x_selecionar, y_selecionar))
-      pygame.display.update()
-     if event.key == pygame.K_UP and y_selecionar =350:
-      y_selecionar = 400
-      SCREEN.fill(BLACK)
-      SCREEN.blit(frase01, (100,100))
-      SCREEN.blit(frase02, (100,200))
-      SCREEN.blit(frase03, (100,300))
-      SCREEN.blit(aguentar, (100,350))
-      SCREEN.blit(reclamar, (100,400))
-      SCREEN.blit(reta_selecionar, (x_selecionar, y_selecionar))
-      pygame.display.update()
-     elif event.key == pygame.K_UP and y_selecionar == 400:
-      y_selecionar = 350
-      SCREEN.fill(BLACK)
-      SCREEN.blit(frase01, (100,100))
-      SCREEN.blit(frase02, (100,200))
-      SCREEN.blit(frase03, (100,300))
-      SCREEN.blit(aguentar, (100,350))
-      SCREEN.blit(reclamar, (100,400))
-      SCREEN.blit(reta_selecionar, (x_selecionar, y_selecionar))
-      pygame.display.update()
+     frase01 = fonte.render("Tudo corria normal... até que", 1, WHITE)
+     SCREEN.blit(frase01, (100,100))
+     pygame.display.update()
+     evento1 = "{}".format(ev.evento)
+     frase02 = fonte.render(evento1, 1, WHITE)
+     SCREEN.blit(frase02, (100,200))
+     pygame.display.update()
+     frase03 = fonte.render("O que você deve fazer?", 1, WHITE)
+     SCREEN.blit(frase03, (100,300))
+     aguentar = pygame.image.load(r'imagens\aguentar.jpg')
+     reclamar = pygame.image.load(r'imagens\reclamar.jpg')
+     aguentar_rect = pygame.Rect(100,350,100,30)
+     reclamar_rect = pygame.Rect(100,400,100,30)
+     
+     pygame.draw.rect(SCREEN,WHITE, aguentar_rect)
+     pygame.draw.rect(SCREEN,WHITE, reclamar_rect)
+     SCREEN.blit(aguentar, (100,350))
+     SCREEN.blit(reclamar, (100,400))
+     x_selecionar = 80
+     y_selecionar = 350
+     reta_selecionar = pygame.image.load(r'imagens\botao_selecionar.png')
+     SCREEN.blit(reta_selecionar, (x_selecionar,y_selecionar))
+     pygame.display.update()
+     while v.looptrabalho == True:
+      for event in pygame.event.get():
+       if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_DOWN and y_selecionar ==  350:
+         y_selecionar = 400
+         SCREEN.fill(BLACK)
+         SCREEN.blit(frase01, (100,100))
+         SCREEN.blit(frase02, (100,200))
+         SCREEN.blit(frase03, (100,300))
+         SCREEN.blit(aguentar, (100,350))
+         SCREEN.blit(reclamar, (100,400))
+         SCREEN.blit(reta_selecionar, (x_selecionar, y_selecionar))
+         pygame.display.update()
+        elif event.key == pygame.K_DOWN and y_selecionar == 400:
+         y_selecionar = 350
+         SCREEN.fill(BLACK)
+         SCREEN.blit(frase01, (100,100))
+         SCREEN.blit(frase02, (100,200))
+         SCREEN.blit(frase03, (100,300))
+         SCREEN.blit(aguentar, (100,350))
+         SCREEN.blit(reclamar, (100,400))
+         SCREEN.blit(reta_selecionar, (x_selecionar, y_selecionar))
+         pygame.display.update()
+        if event.key == pygame.K_UP and y_selecionar == 350:
+         y_selecionar = 400
+         SCREEN.fill(BLACK)
+         SCREEN.blit(frase01, (100,100))
+         SCREEN.blit(frase02, (100,200))
+         SCREEN.blit(frase03, (100,300))
+         SCREEN.blit(aguentar, (100,350))
+         SCREEN.blit(reclamar, (100,400))
+         SCREEN.blit(reta_selecionar, (x_selecionar, y_selecionar))
+         pygame.display.update()
+        elif event.key == pygame.K_UP and y_selecionar == 400:
+         y_selecionar = 350
+         SCREEN.fill(BLACK)
+         SCREEN.blit(frase01, (100,100))
+         SCREEN.blit(frase02, (100,200))
+         SCREEN.blit(frase03, (100,300))
+         SCREEN.blit(aguentar, (100,350))
+         SCREEN.blit(reclamar, (100,400))
+         SCREEN.blit(reta_selecionar, (x_selecionar, y_selecionar))
+         pygame.display.update()
       #identificar qual botao esta sendo apertado:
-     if event.key == pygame.K_RETURN:
-      localdoy = y_selecionar
-      if localdoy == 350:
+      
+        if event.key == pygame.K_RETURN:
+         localdoy = y_selecionar
+         if localdoy == 350:
              v.depre += 1
              frase04 = fonte.render("You're feeling worst", 1, WHITE)
              SCREEN.blit(frase04, (100,500))
@@ -106,13 +138,14 @@ def trabalho():
              SCREEN.fill(BLACK)
              pygame.display.update()
              pygame.mixer.Sound.stop(trabalho_music)
+             v.looptrabalho = False
              v.uber = False
              v.onibus = False
              v.carro = False
              v.pe = False
              v.no_trabalho = False
              v.jantar = True
-      If localdoy == 400:
+         if localdoy == 400:
              v.depre -= 1
              frase04 = fonte.render("You're feeling better", 1, WHITE)
              SCREEN.blit(frase04, (100,500))
@@ -121,12 +154,10 @@ def trabalho():
              SCREEN.fill(BLACK)
              pygame.display.update()
              pygame.mixer.Sound.stop(trabalho_music)
+             v.looptrabalho = False
              v.uber = False
              v.onibus = False
              v.carro = False
              v.pe = False
              v.no_trabalho = False
              v.jantar = True
-
-     
-     
