afffba6ca9e62cc8c178090a9e00e32a09b905b5
Eduardo Pinho <86359636+ShyrTheFirst@users.noreply.github.com>
2021-11-11 11:04:56 -0300

    Delete english directory

--- english/evento_aleatorio.py
+++
@@ -1,9 +0,0 @@
-import random
-
-lista_random = ["a coworker trashtalk you",
-                     "Your boss steal all your work's credit",
-                     "Computer stop working while you're using it",
-                     "You're feeling overhelming by your coworkers",
-                     "You hear gossip about you",
-                     "You're being bullied by your coworkers"]
-evento = random.choice(lista_random)
