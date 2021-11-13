57d19f0b05f51e976426266f921fe6723d4b2e61
Eduardo Pinho <86359636+ShyrTheFirst@users.noreply.github.com>
2021-11-13 09:37:09 -0300

    atualização 2.0.3

--- Living With 2.0/mainmenu.py
+++ Living With 2.0/mainmenu.py
@@ -60,21 +60,39 @@ def quitgame():
 
 def menu():
     relogio = pygame.time.Clock()
-    tela = pygame.display.set_mode((800,600))
+    largura = 800
+    altura = 600
+    tela = pygame.display.set_mode((largura,altura))
     branco = (255,255,255)
     preto = (0,0,0)
     vermelho = (255,0,0)
+
+    #info botoes#
     cor_ativa_sair = branco
     cor_ativa_iniciar = branco
+    cor_ativa_opcoes = branco
     estilo_ativo_sair = padrao
     estilo_ativo_iniciar = padrao
+    estilo_ativo_opcoes = padrao
+    #info botoes confirmacao#
+    cor_ativa_sim = branco
+    cor_ativa_nao = branco
+    estilo_ativo_sim = padrao
+    estilo_ativo_nao = padrao
+    #info botoes opcoes#
+    cor_ativa_r800_600 = branco
+    estilo_ativo_r800_600 = padrao
+    cor_ativa_r1024_768 = branco
+    estilo_ativo_r1024_768 = padrao
+    
     tela.fill(preto)
     pygame.display.update()
     relogio.tick(60)
     game_runing = True
+    saindo = False
+    config = False
     titulo1 = titulo_movendo(tela,preto,branco)
     pygame.display.flip()
-    first_open = True
     
     
     
@@ -83,18 +101,8 @@ def menu():
         titulo = fonte_principal.render_to(tela,(300,30),"Living With", fgcolor=branco, bgcolor=None,style=padrao,size=80)
         sair = fonte_principal_pequena.render_to(tela,(70,390),"Sair",fgcolor=cor_ativa_sair, bgcolor=None,style=estilo_ativo_sair,size=30)
         iniciar = fonte_principal_pequena.render_to(tela,(70,270),"Iniciar",fgcolor=cor_ativa_iniciar, bgcolor=None,style=estilo_ativo_iniciar,size=30)
-        while first_open:
-            fade = pygame.Surface((800,600))
-            tela.fill(branco)
-            fade.fill(branco)
-            for alpha in range(255,0, -1):
-                fade.set_alpha(alpha)
-                tela.fill(preto)
-                tela.blit(fade,(0,0))
-                pygame.display.update()
-                pygame.time.delay(5)
-            first_open = False
-
+        opcoes = fonte_principal_pequena.render_to(tela,(70,330),"Opcoes",fgcolor=cor_ativa_opcoes, bgcolor=None,style=estilo_ativo_opcoes,size=30)
+        
             
         pygame.display.flip()
         quitgame()
@@ -113,16 +121,90 @@ def menu():
         else:
             cor_ativa_iniciar = branco
             estilo_ativo_iniciar = padrao
+
+        if opcoes.collidepoint(posicao_mouse):
+            cor_ativa_opcoes = vermelho
+            estilo_ativo_opcoes = ob_sub
+        else:
+            cor_ativa_opcoes = branco
+            estilo_ativo_opcoes = padrao
                 
             
             
         if pygame.mouse.get_pressed() == (1,0,0):
             posicao_mouse = pygame.mouse.get_pos()
             if sair.collidepoint(posicao_mouse):
-                pygame.quit()
-                sys.exit()
+                saindo = True
+                while saindo:
+                    quitgame()
+                    tela.fill(preto)
+                    tem_certeza = fonte_principal_pequena.render_to(tela,(250,250),"Tem certeza?",fgcolor=branco, bgcolor=None,style=padrao,size=50)
+                    sim_certeza = fonte_principal_pequena.render_to(tela,(270,350),"Sim",fgcolor=cor_ativa_sim, bgcolor=None,style=estilo_ativo_sim,size=30)
+                    nao_certeza = fonte_principal_pequena.render_to(tela,(450,350),"Nao",fgcolor=cor_ativa_nao, bgcolor=None,style=estilo_ativo_nao,size=30)
+                    pygame.display.flip()
+                    posicao_mouse = pygame.mouse.get_pos()
+                    if sim_certeza.collidepoint(posicao_mouse):
+                        cor_ativa_sim = vermelho
+                        estilo_ativo_sim = ob_sub
+                    else:
+                        cor_ativa_sim = branco
+                        estilo_ativo_sim = padrao
+
+                    if nao_certeza.collidepoint(posicao_mouse):
+                        cor_ativa_nao = vermelho
+                        estilo_ativo_nao = ob_sub
+                    else:
+                        cor_ativa_nao = branco
+                        estilo_ativo_nao = padrao
+                        
+                    if pygame.mouse.get_pressed() == (1,0,0):
+                        posicao_mouse = pygame.mouse.get_pos()
+                        if sim_certeza.collidepoint(posicao_mouse):
+                            pygame.quit()
+                            sys.exit()
+                            break
+                        if nao_certeza.collidepoint(posicao_mouse):
+                            saindo = False
             if iniciar.collidepoint(posicao_mouse):
                 print("start")
+
+            if opcoes.collidepoint(posicao_mouse):
+                config = True
+                while config:
+                    quitgame()
+                    tela.fill(preto)
+                    resolucao = fonte_principal_pequena.render_to(tela,(180,130),"Escolha a resolucao:",fgcolor=branco, bgcolor=None,style=padrao,size=50)
+                    r800_600 = fonte_principal_pequena.render_to(tela,(330,220),"800x600",fgcolor=cor_ativa_r800_600, bgcolor=None,style=estilo_ativo_r800_600,size=30)
+                    r1024_768 = fonte_principal_pequena.render_to(tela,(330,280),"1024x768",fgcolor=cor_ativa_r1024_768, bgcolor=None,style=estilo_ativo_r1024_768,size=30)
+                    pygame.display.flip()
+                    posicao_mouse = pygame.mouse.get_pos()
+                    if r800_600.collidepoint(posicao_mouse):
+                        cor_ativa_r800_600 = vermelho
+                        estilo_ativo_r800_600 = ob_sub
+                    else:
+                        cor_ativa_r800_600 = branco
+                        estilo_ativo_r800_600 = padrao
+
+                    if r1024_768.collidepoint(posicao_mouse):
+                        cor_ativa_r1024_768 = vermelho
+                        estilo_ativo_r1024_768 = ob_sub
+                    else:
+                        cor_ativa_r1024_768 = branco
+                        estilo_ativo_r1024_768 = padrao
+                        
+                    if pygame.mouse.get_pressed() == (1,0,0):
+                        posicao_mouse = pygame.mouse.get_pos()
+                        if r800_600.collidepoint(posicao_mouse):
+                            largura = 800
+                            altura = 600
+                            tela = pygame.display.set_mode((largura,altura))
+                            config = False
+                        if r1024_768.collidepoint(posicao_mouse):
+                            largura = 1024
+                            altura = 768
+                            tela = pygame.display.set_mode((largura,altura))
+                            config = False
+                    
         
 
 menu()
